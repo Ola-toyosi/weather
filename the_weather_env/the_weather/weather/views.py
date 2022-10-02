a0995a1581768b98f2da92c258efc6418e443766
toyosi <toyosiola@yaoo.com>
2022-10-01 21:37:51 -0700

    Added openweather map api and some built in cities . Replace "YOUR_APP_KEY" with api

--- the_weather_env/the_weather/weather/views.py
+++ the_weather_env/the_weather/weather/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+import requests
+from .models import City
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'weather/index.html') #returns the index.html template
+    appid = '57446644697e84d1d693192b0a07fc67'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=YOUR_APP_KEY'
+
+    cities = City.objects.all()  # return all cities in the database
+
+    weather_data = []
+
+    for city in cities:
+        city_weather = requests.get(
+            url.format(city)).json()  # request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+        print(weather)
+
+        weather_data.append(weather)  # add the data for the current city into the list
+
+    context = {'weather_data': weather_data}
+
+    return render(request, 'weather/index.html', context)  # returns the index.html template
