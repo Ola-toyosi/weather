27d989c9d10fd3f9f6572868e87fec2badc6abbe
toyosi <toyosiola@yaoo.com>
2022-10-01 22:13:56 -0700

    Created Form to allow addition of more cities

---
+++ the_weather_env/the_weather/weather/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm, TextInput
+from .models import City
+
+
+class CityForm(ModelForm):
+    class Meta:
+        model = City
+        fields = ['name']
+        widgets = {
+            'name': TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'City name'})
+        }   # updates the input class to have the correct Bootstrap class and placeholder
--- the_weather_env/the_weather/weather/views.py
+++ the_weather_env/the_weather/weather/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 
 
 # Create your views here.
-
 def index(request):
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=YOUR_APP_KEY'
 
     cities = City.objects.all()  # return all cities in the database
+
+    if request.method == 'POST':  # only true if form is submitted
+        form = CityForm(request.POST)  # add actual request data to form for processing
+        form.save()  # will validate and save if validate
+
+    form = CityForm()
 
     weather_data = []
 
@@ -26,6 +32,6 @@ def index(request):
 
         weather_data.append(weather)  # add the data for the current city into the list
 
-    context = {'weather_data': weather_data}
+    context = {'weather_data': weather_data, 'form': form}
 
     return render(request, 'weather/index.html', context)  # returns the index.html template
